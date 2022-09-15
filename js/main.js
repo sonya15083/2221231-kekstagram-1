17f39f16b9ffb186b0319cab57b80c405a7d61db
sonya15083 <104288586+sonya15083@users.noreply.github.com>
2022-09-15 19:04:22 +0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function getRandom(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function checksLength(string,maxLength){
+    if(string.length<=maxLength){
+        return true;
+    }
+    return false;
+}
