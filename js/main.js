b73ab283e7f1ba4d331ed1f6a8453eaf46a6b9e8
sonya15083 <104288586+sonya15083@users.noreply.github.com>
2022-10-03 13:07:21 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -55,19 +55,4 @@ function createDescription(id){
 
 const array=Array.from({length:photosAmount}).map((value, index)=>createDescription(index+1));
 
-function getRandom(min, max) {
-  if (min>=0 && min<max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  return 'Упс, что-то не так...';
-}
-
-function checksLength(string,maxLength){
-  if(string.length<=maxLength){
-    return true;
-  }
-  return false;
-}
-getRandom(2,5);
-checksLength('uyciygct',5);
 //master
