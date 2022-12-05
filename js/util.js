6f9b9302eee17806b53a5db5544614e984b74a33
sonya15083 <104288586+sonya15083@users.noreply.github.com>
2022-12-05 15:47:55 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -5,9 +5,22 @@ function getRandomPositiveInteger (a, b) {
   return Math.floor(result);
 }
 
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '15px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+  
+ 
 function checkStringLength (string, length) {
   return string.length <= length;
 }
 checkStringLength('Нужно вызвать!',70);
 
-export {getRandomPositiveInteger,checkStringLength};
+export {getRandomPositiveInteger,checkStringLength,showAlter};
