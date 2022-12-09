310352efaaddf9e671f0ac0528d3267f3a38c22c
sonya15083 <104288586+sonya15083@users.noreply.github.com>
2022-12-09 11:32:57 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -16,11 +16,26 @@ const showAlert = (message) => {
   alertContainer.style.fontSize = '15px';
   alertContainer.style.textAlign = 'center';
   alertContainer.style.backgroundColor = 'red';
-  
+ 
+const debounce = (callback, timeoutDelay = DELAY) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
  
 function checkStringLength (string, length) {
   return string.length <= length;
 }
 checkStringLength('Нужно вызвать!',70);
 
-export {getRandomPositiveInteger,checkStringLength,showAlter};
+function shuffle(array) {
+  array = array.slice();
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+export {getRandomPositiveInteger,checkStringLength,showAlter,debounce,shuffle};
