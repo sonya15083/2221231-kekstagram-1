33f5676e01b6585b5f6c7045e1d2faa725719d25
sonya15083 <104288586+sonya15083@users.noreply.github.com>
2022-12-19 13:21:23 +0500

    Update form-val.js

--- js/form-val.js
+++ js/form-val.js
@@ -20,12 +20,12 @@ const validateForm = (form, hashtags, comment) => {
     const uniqueHashtags = [...new Set(hashtagsList)];
     return value === '' || hashtagsList.every((hashtag) => hashtagRule.test(hashtag)) && hashtagsList.length <= 5 && hashtagsList.length === uniqueHashtags.length;
   },
-    'Уникальные хештеги, каждый не более 20 символов, должны быть разделены пробелом');
+  'Уникальные хештеги, каждый не более 20 символов, должны быть разделены пробелом');
   pristine.addValidator(comment, () => {
     const value = comment.value;
     return checkStringLength(value, 140);
-  }, 
-    'Комментарий не более 140 символов');
+  },
+  'Комментарий не более 140 символов');
   return pristine.validate();
 };
 
